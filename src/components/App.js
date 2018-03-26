747713e8e2ed52e4949eb4625d8db1a31c25311b
Ania <ania.makes@gmail.com>
2018-03-26 16:47:47 +0100

    repo setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return {
+    <div>
+      Our App goes here!
+    </div>
+  };
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '.components/App';
+
+import thunkMiddle from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware (
+  thunkMiddleware
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
