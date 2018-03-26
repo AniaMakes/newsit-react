ffeeba92032232cf04d3b2f22221803f0d36e4a7
OlenaKashuba <amely.jazz@gmail.com>
2018-03-26 16:59:38 +0100

    Set up webpack and eslint

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: require.resolve('babel-loader')
+      }, {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: require.resolve('eslint-loader'),
+        options: {
+          fix: true,
+        }
+      }, {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
