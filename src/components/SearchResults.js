01d6f33a40b83bfaac29a1312d803d1e452f9012
yevhensydorov <assid@ukr.net>
2018-03-30 17:10:42 +0100

    switch to default page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Story from './Story';
 
-const SearchResults = ({articles, searchResults}) => {
+const SearchResults = (props) => {
+	const {articles} = props;
+	const {searchResults} = props.props;
 
 	return (
-		<section>
-			{/*			<h3> Found: </h3>
-*/}			{articles.map((storyData, i) => {
+		<section className={searchResults ? 'stories-show' : 'hidden'}>
+			<h3> Found: </h3>		
+			{articles.map((storyData, i) => {
 				return <Story
 					storyData={storyData}
 					key={i}
