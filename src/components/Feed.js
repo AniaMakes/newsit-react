b8a4afd014eb112447dc7039b569d0cf3eeb0b1a
Ania <ania.makes@gmail.com>
2018-04-02 17:34:29 +0100

    number of categories on home page now match the user preferences saved in Local Storage

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -8,13 +8,64 @@ class Feed extends React.Component {
     	super(props);
  	}
 	componentDidMount() {
-		categories.forEach((category) => {
-			this.props.getNews(category);
-		}, this);
+
+		const JSONfromLocalStorage = localStorage.getItem('news preferences');
+
+		const objectFromLocalStorage =  JSON.parse(JSONfromLocalStorage);
+
+		let preferredCategories = [];
+
+		if(objectFromLocalStorage != null){
+
+			Object.keys(objectFromLocalStorage.preferecesObject.categoryPicker).forEach(key => {
+				if(objectFromLocalStorage.preferecesObject.categoryPicker[key]){
+					preferredCategories.push(key);
+				}
+			});
+		}
+
+		console.log('pref Cat ', preferredCategories);
+
+		if (preferredCategories.length > 0) {
+			preferredCategories.forEach((category) => {
+				this.props.getNews(category);
+			}, this);
+
+		} else {
+			categories.forEach((category) => {
+				this.props.getNews(category);
+			}, this);
+		}
+
 	};
 
 	render() {
-		const groups = categories.map((category, i) => {
+
+		const JSONfromLocalStorage = localStorage.getItem('news preferences');
+
+		const objectFromLocalStorage =  JSON.parse(JSONfromLocalStorage);
+
+		let preferredCategories = [];
+
+		if(objectFromLocalStorage != null){
+
+			Object.keys(objectFromLocalStorage.preferecesObject.categoryPicker).forEach(key => {
+				if(objectFromLocalStorage.preferecesObject.categoryPicker[key]){
+					preferredCategories.push(key);
+				}
+			});
+		}
+
+		let categoriesShown;
+
+		if(preferredCategories.length > 0){
+			categoriesShown = preferredCategories;
+		} else {
+			categoriesShown = categories;
+		}
+		// this doesn't account for those who deliberately don't check any categories in preferences!
+
+		const groups = categoriesShown.map((category, i) => {
 			return <Group
 				category={category}
 				data={this.props.news.news[category]}
