fd90c36ff8224834139e9cfe597804fb3470a118
Ania <ania.makes@gmail.com>
2018-04-04 17:02:05 +0100

    delete redundant line

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Error = ({category, error}) => (
 	<div className='error-component'>
 		<h2>Category: {category}</h2>
-		<p>This is an error</p>
 		<p>Error: {error}</p>
 	</div>
 );
