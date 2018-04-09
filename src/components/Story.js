f50ff9922ce194d3348f9e1fbb561888b015f092
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-04-09 12:46:24 +0100

    add timestamp to stories

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 import '../../styles/Story.scss';
 
 const Story = ({storyData}) => {
-	const {url, title, description} = storyData;
+	const {url, title, description,publishedAt} = storyData;
+	const temp = new Date(publishedAt);
+	const date = temp.toDateString();
+	const tempTimeString = temp.toTimeString();
+	const tempTime = tempTimeString.split(' ');
+	const time = tempTime[0].slice(0,5);
+	const timeDate = time + ', ' + date;
 	return (
 		<article className='story'>
 			<a href={url} target='_blank'>
@@ -13,6 +19,9 @@ const Story = ({storyData}) => {
 			</a>
 			<p className='story-description'>
 				{description}
+			</p>
+			<p className='story-time'>
+				{timeDate}
 			</p>
 		</article>
 
